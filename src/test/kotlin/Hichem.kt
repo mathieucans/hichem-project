61ce4fce09775712f82ccfd31c5be161beb02c29
mathieu cans <mathieucans@gmail.com>
2022-05-10 15:27:37 +0200

    create project with failed test

---
+++ src/test/kotlin/Hichem.kt
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Hichem {
+    @Test
+    fun `don't like tdd`(){
+        assertThat(true).isFalse();
+    }
+}
